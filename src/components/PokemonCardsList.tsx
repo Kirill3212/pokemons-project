55802053c5dc1b1b6b76ee49c97af2d28c6153f6
Kirill Terekhin <kirill.terekhin.98@bk.ru>
2024-01-29 12:02:12 +0300

    adding show more functionality

--- src/components/PokemonCardsList.tsx
+++ src/components/PokemonCardsList.tsx
@@ -1,56 +1,92 @@
-import { Grid, Skeleton, Image, Text, Flex } from "@chakra-ui/react";
+import { useState } from "react";
+import {
+  Grid,
+  Skeleton,
+  Image,
+  Text,
+  Flex,
+  VStack,
+  Divider,
+} from "@chakra-ui/react";
 import { SinglePokemonResponse } from "../types/pokemonData";
 import errorLoading from "../assets/errorLoading.gif";
+import pokeballHeartActive from "../assets/pokeballHeartActive.png";
 import { useGetPokemonsQuery } from "../api/api";
 import PokemonCard from "./PokemonCard";
 
 const PokemonCardsList = () => {
-  const { data: pokemons, isLoading, isError } = useGetPokemonsQuery(16);
+  const [cardsToShow, setCardsToShow] = useState(20);
+  const {
+    data: pokemons,
+    isLoading,
+    isError,
+  } = useGetPokemonsQuery(cardsToShow);
 
   return (
-    <Grid
-      mt={10}
-      gap={5}
-      templateColumns={{
-        base: "repeat(1, 1fr)",
-        sm: "repeat(2, 1fr)",
-        md: "repeat(3, 1fr)",
-        lg: "repeat(4, 1fr)",
-      }}
-      position={"relative"}
-      justifyItems={"center"}
-    >
-      {isLoading && (
-        <>
-          <Skeleton height={"325px"} width={"230px"} borderRadius={10} />
-          <Skeleton height={"325px"} width={"230px"} borderRadius={10} />
-          <Skeleton height={"325px"} width={"230px"} borderRadius={10} />
-          <Skeleton height={"325px"} width={"230px"} borderRadius={10} />
-        </>
-      )}
+    <VStack>
+      <Grid
+        mt={10}
+        gap={5}
+        templateColumns={{
+          base: "repeat(1, 1fr)",
+          sm: "repeat(2, 1fr)",
+          md: "repeat(3, 1fr)",
+          lg: "repeat(4, 1fr)",
+        }}
+        position={"relative"}
+        justifyItems={"center"}
+      >
+        {isLoading && (
+          <>
+            <Skeleton height={"325px"} width={"230px"} borderRadius={10} />
+            <Skeleton height={"325px"} width={"230px"} borderRadius={10} />
+            <Skeleton height={"325px"} width={"230px"} borderRadius={10} />
+            <Skeleton height={"325px"} width={"230px"} borderRadius={10} />
+          </>
+        )}
 
-      {isError && (
-        <Flex
-          flexDirection={"column"}
-          position={"absolute"}
-          width={"320px"}
-          alignItems={"center"}
-        >
-          <Text fontSize={{ base: 14, md: 18, lg: 21 }} mb={7}>
-            Oops! Something went wrong &#128531;
-          </Text>
-          <Image
-            src={errorLoading}
-            width={{ base: "160px", md: "200px", lg: "250px" }}
-          ></Image>
-        </Flex>
-      )}
+        {isError && (
+          <Flex
+            flexDirection={"column"}
+            position={"absolute"}
+            width={"320px"}
+            alignItems={"center"}
+          >
+            <Text fontSize={{ base: 14, md: 18, lg: 21 }} mb={7}>
+              Oops! Something went wrong &#128531;
+            </Text>
+            <Image
+              src={errorLoading}
+              width={{ base: "160px", md: "200px", lg: "250px" }}
+            ></Image>
+          </Flex>
+        )}
 
-      {pokemons?.results &&
-        pokemons.results.map((pokemon: SinglePokemonResponse) => (
-          <PokemonCard key={pokemon.name} pokemon={pokemon} />
-        ))}
-    </Grid>
+        {pokemons?.results &&
+          pokemons.results.map((pokemon: SinglePokemonResponse) => (
+            <PokemonCard key={pokemon.name} pokemon={pokemon} />
+          ))}
+      </Grid>
+      <Flex mt={6} width={"400px"} alignItems={"center"}>
+        <Divider orientation="horizontal" mr={3} borderWidth="1px" />
+        <Image
+          src={pokeballHeartActive}
+          cursor={"pointer"}
+          mt={5}
+          transition={"0.3s"}
+          _hover={{ transform: "scale(1.2)" }}
+          width={"30px"}
+          onClick={() => setCardsToShow(cardsToShow + 8)}
+          pb={4}
+        ></Image>
+        <Divider
+          orientation="horizontal"
+          color={"red"}
+          ml={3}
+          borderWidth="1px"
+        />
+      </Flex>
+    </VStack>
   );
 };
 
