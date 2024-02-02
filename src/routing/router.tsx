eb2e130c0df05774a44a7850a4e1698fd8e7a9b5
Kirill Terekhin <kirill.terekhin.98@bk.ru>
2024-02-02 14:25:29 +0300

    adding lazy routing

--- src/routing/router.tsx
+++ src/routing/router.tsx
@@ -1,4 +1,4 @@
-import { Suspense } from "react";
+import { Suspense, lazy } from "react";
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -10,14 +10,14 @@ import Loading from "../components/Loading";
 import { PrivateRoute } from "./privateRoute";
 
 // Pages
-import Home from "../pages/Home";
-import NotFound from "../pages/NotFound";
-import SingleCard from "../pages/singleCard/SingleCard";
-import Favorites from "../pages/Favorites";
-import History from "../pages/History";
-import SignIn from "../pages/auth/SignIn";
-import SignUp from "../pages/auth/SignUp";
-import Search from "../pages/Search";
+const Home = lazy(() => import("../pages/Home"));
+const NotFound = lazy(() => import("../pages/NotFound"));
+const SingleCard = lazy(() => import("../pages/singleCard/SingleCard"));
+const Favorites = lazy(() => import("../pages/Favorites"));
+const History = lazy(() => import("../pages/History"));
+const SignIn = lazy(() => import("../pages/auth/SignIn"));
+const SignUp = lazy(() => import("../pages/auth/SignUp"));
+const Search = lazy(() => import("../pages/Search"));
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
