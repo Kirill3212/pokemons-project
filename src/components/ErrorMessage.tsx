385f8abf5a9043942c5f8611bb9bfce6e9e341d8
Kirill Terekhin <kirill.terekhin.98@bk.ru>
2024-02-07 19:35:48 +0300

    adding context error message

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,15 +1,20 @@
+import { useContext } from "react";
+import { ErrorMessageContext } from "./Layout";
+
 import { VStack, Image, Heading, Text } from "@chakra-ui/react";
 import stumblingPika from "../assets/stumblingPika.gif";
 
 const ErrorMessage = () => {
+  const errMessage = useContext(ErrorMessageContext);
+
   return (
     <VStack width={"100%"} height={"100vh"} justifyContent={"center"}>
       <VStack mb="150px">
         <Image src={stumblingPika} width={"130px"} />
         <Heading fontSize={20} color={"green.600"}>
-          OH Nooo...Oops!
+          {errMessage.header}
         </Heading>
-        <Text fontSize={18}>Sorry, something went wrong there. Try again</Text>
+        <Text fontSize={18}>{errMessage.message}</Text>
       </VStack>
     </VStack>
   );
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,24 @@
 import { Outlet } from "react-router-dom";
+import { createContext } from "react";
 
 import Header from "./Header";
 import Footer from "./Footer";
+
+// Context
+const errMessage = {
+  header: "OH Nooo...Oops!",
+  message: "Sorry, something went wrong there. Try again",
+};
+export const ErrorMessageContext = createContext(errMessage);
 
 const Layout = () => {
   return (
     <div className="site-wrapper">
       <Header />
       <main>
-        <Outlet />
+        <ErrorMessageContext.Provider value={errMessage}>
+          <Outlet />
+        </ErrorMessageContext.Provider>
       </main>
       <Footer />
     </div>
